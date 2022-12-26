b00dd162abd7c5038963390671e4f11514a21b81
ASnider <81870185+anthoneees@users.noreply.github.com>
2022-12-25 23:05:53 -0600

    Adding services by port

--- PortScanner.py
+++ PortScanner.py
@@ -8,22 +8,15 @@ print("Scanning " + target)
 print("_" * 60)
 def port_scanner(port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
+    s.settimeout(.5)
     try:
-        result = s.connect_ex((target, port))
-        if result == 0:
-            serviceName = socket.getservbyport(port, "tcp")
-            print("Port :", port, "is open. ")
-            print("Service running at port %d is %s"%(port, serviceName))
-        else:
-            pass
+        result = s.connect((target, port))
+        serviceName = socket.getservbyport(port, "tcp")
+        print("Port :", port, "is open. ")
+        print("Service running at port %d is %s"%(port, serviceName))
         result.close()
-    except socket.error:
-        print("socket.error exception")
-        sys.exit()
-    except socket.gaierror:
-        print("socket.gaierror exception")
-        sys.exit()
+    except:
+        pass
 count=1
 for x in range(1,5000):
     t=threading.Thread(target=port_scanner,kwargs={'port':count})
